8ad268be1ad1589dab1b7a5027226e0eb4b11c8e
Dastanbek <muktaraliuuludastanbek@gmail.com>
2023-11-02 01:53:37 +0600

    second commit

--- lesson13_game_part_3.dart
+++ lesson13_game_part_3.dart
@@ -1,39 +1,58 @@
 import 'dart:io';
 import 'dart:math';
 
-void main(List<String> args) {
+void main() {
+  start_game();
+}
+
+start_game() {
   print("Выберите режим игры: 1 или 2");
   print("1-режим. Угадывать будете вы.");
   print("2-режим. Угадывать будет компьютер");
-  int a = int.parse(stdin.readLineSync()!);
-  if (a == 1) {
+
+  String gameMode = stdin.readLineSync()!; // gameMode - режим игры
+
+  if (gameMode == '1') {
     int x = 1 + Random().nextInt(100);
     computer(x);
-  } else if (a == 2) {
-    print("Загадайте число в диапазоне[0-100)");
-    int b = int.parse(stdin.readLineSync()!);
-    print("Выбирайте способ угадывания:");
-    print("1-режим. Рандомный метод.");
-    print("2-режим. Бинарный метод");
-    int a = int.parse(stdin.readLineSync()!);
-    if (a == 1) {
-      zadacha2_1(b);
-    } else if (a == 2) {
-      zadacha2_2(b);
-    }
+  } else if (gameMode == '2') {
+    print("Загадайте число в диапазоне[0-100) и затем");
+    computerModeHelper();
+  } else {
+    start_game();
+  }
+}
+
+computerModeHelper() {
+  print("Выбирайте способ угадывания:");
+  print("1-режим. Рандомный метод.");
+  print("2-режим. Бинарный метод");
+
+  String a = stdin.readLineSync()!;
+
+  if (a == '1') {
+    random_search();
+  } else if (a == '2') {
+    binary_search();
+  } else {
+    computerModeHelper();
   }
 }
 
 void computer(int chislo) {
   int count = 0;
-  print("Отгадайте число которое загадал компьютер:");
   int a = -1;
+
+  print("Отгадайте число которое загадал компьютер:");
+
   while (a != chislo) {
     print("-------------------------------");
     a = int.parse(stdin.readLineSync()!);
+
     if (a > 0 && a <= 100) {
       count++;
       print("Попытка ------ $count");
+
       if (a > chislo) {
         print("greater ----- ваше число больше");
       } else if (a < chislo) {
@@ -48,38 +67,60 @@ void computer(int chislo) {
   print("Вы нашли загаданное число компа за $count попыток");
 }
 
-int zadacha2_1(int chislo) {
-  int counter = 1;
+int random_search() {
+  int counter = 0;
   int min = 0;
   int max = 100;
-  int a = -1;
-  while (chislo != a) {
+  int randomChislo = -1;
+  String userAnswer = "";
+
+  while (userAnswer != "=") {
     counter++;
-    a = min + Random().nextInt(max - min);
-    if (a < chislo) {
-      min = a + 1;
-    } else {
-      max = a;
+    randomChislo = min + Random().nextInt(max - min);
+    print("vashe chislo -$randomChislo?");
+    userAnswer = stdin.readLineSync()!;
+
+    if (userAnswer == "+") {
+      min = randomChislo + 1;
+    } else if (userAnswer == "-") {
+      max = randomChislo;
+    } else if (userAnswer != "=") {
+      counter--;
+      print('Ответьте символами "+" и "-"');
     }
   }
+  print("vashe chislo - $randomChislo.");
   print("kolichestvo popytok(rondom_search)=$counter");
+
   return counter;
 }
 
-int zadacha2_2(int chislo) {
-  int count = 1;
+int binary_search() {
+  int count = 0;
   int min = 0;
   int max = 100;
-  int mid = (min + max) ~/ 2;
-  while (chislo != mid) {
-    if (chislo > mid) {
-      min = mid;
-    } else {
-      max = mid;
-    }
+  int mid = -1;
+  String userAnswer = "";
+
+  while (userAnswer != "=") {
     mid = (min + max) ~/ 2;
     count++;
+    print("vashe chislo -$mid?");
+
+    userAnswer = stdin.readLineSync()!;
+
+    if (userAnswer == "+") {
+      min = mid;
+    } else if (userAnswer == "-") {
+      max = mid;
+    } else if (userAnswer != "=") {
+      count--;
+      print('Ответьте символами "+" и "-"');
+    }
   }
-  print("kolichestvo popytok(binary_search)=$count");
+
+  print("vashe chislo - $mid.");
+  print("kolichestvo popytok(rondom_search)=$count");
+
   return count;
 }
